6cdaab80e1ce913d01ced9355d5ec469721a41aa
Bartixxx32 <mcbplay1@gmail.com>
2026-03-25 05:09:47 +0100

    Redirect to hit worker

--- functions/hit.js
+++ functions/hit.js
@@ -1,22 +1,6 @@
 export async function onRequest(context) {
-  const { request } = context;
-
   try {
-    // Wywołanie Workera pod nowym URL (oparb.bartixxx.workers.dev/hit)
-    // Forwardujemy nagłówki, żeby Worker dostał IP końcowego użytkownika
-    await fetch("https://oparb.bartixxx.workers.dev/hit", {
-      method: request.method,
-      headers: request.headers
-    });
-
-    // Zawsze zwracamy OK – żeby frontend/apka nie zauważyła zmiany
-    return new Response("OK", {
-      status: 200,
-      headers: {
-        "Content-Type": "text/plain",
-        "Access-Control-Allow-Origin": "*"
-      }
-    });
+    return Response.redirect("https://oparb.bartixxx.workers.dev/hit", 302);
   } catch (err) {
     return new Response("Error", { status: 500 });
   }
