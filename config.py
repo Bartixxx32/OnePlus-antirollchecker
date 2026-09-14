344d0c27e55d6b80c795ba549560b5f86d8b268b
Bartixxx32 <mcbplay1@gmail.com>
2026-09-14 21:43:28 +0200

    up

--- config.py
+++ config.py
@@ -487,13 +487,6 @@ DEVICE_METADATA: Dict[str, DeviceMeta] = {
             "VN": "CPH2525VN"
         },
     },
-
-    "Find X8 Ultra": {
-        "name": "Oppo Find X8 Ultra",
-        "models": {
-            "CN": "PKJ110"
-        },
-    },
     "Find N5": {
         "name": "Oppo Find N5",
         "models": {
@@ -502,8 +495,7 @@ DEVICE_METADATA: Dict[str, DeviceMeta] = {
             "APC": "CPH2671",
             "ID": "CPH2671",
             "MX": "CPH2671",
-            "TH": "CPH2671",
-            "CN": "PKV110"
+            "TH": "CPH2671"
         },
     },
     "Find N3": {
